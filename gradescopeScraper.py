401d7a9d7c425e9746b228b7a9605c6b422050ad
Wu, Emily Jaling <ewu64@gatech.edu>
2024-03-01 11:39:32 -0500

    differentiate between campus/hybrid and online submissions. write output to a csv

--- gradescopeScraper.py
+++ gradescopeScraper.py
@@ -1,47 +1,80 @@
+import csv
 import requests
 from bs4 import BeautifulSoup
 from pprint import pprint
 import numpy as np
+import os
 
-def countSubmissions(filename):
-    with open(filename) as infile:
+
+def countSubmissions(campusFile, onlineFile):
+    # Process campus/hybrid submissions
+    with open(campusFile) as infile:
         soup = BeautifulSoup(infile, "html.parser")
 
     rows = [row for row in soup.find_all("tr")]
-    tas = {}
+    # tas = {name: {count: 0, section: "O"}}
+    tas = {} 
     for row in rows:
         items = [item.text for item in row.find_all("td")]
         if len(items) > 2 and items[2]:
             name = items[2]
-            count = tas.get(name, 0)
-            tas[name] = count + 1
-        else:
-            continue
+            tas[name] = tas.get(name, {"count": 0, "section": "C"})
+            tas[name]["count"] += 1
+    
+    # Process online submissions
+    with open(onlineFile) as infile:
+        soup = BeautifulSoup(infile, "html.parser")
+
+    rows = [row for row in soup.find_all("tr")]
+    for row in rows:
+        items = [item.text for item in row.find_all("td")]
+        if len(items) > 2 and items[2]:
+            name = items[2]
+            tas[name] = tas.get(name, {"count": 0, "section": "O"})
+            tas[name]["count"] += 1
     return tas
 
-def countEfficiencies(filename):
-    with open(filename) as infile:
+def countEfficiencies(campusFile, onlineFile):
+    # Process campus/hybrid submissions
+    with open(campusFile) as infile:
         soup = BeautifulSoup(infile, "html.parser")
+
     rows = [row for row in soup.find_all("tr")]
-    tas = {}
+    # tas = {name: {count: 0, section: "O"}}
+    tas = {} 
     for row in rows:
         items = [item.text for item in row.find_all("td")]
         if len(items) > 3 and items[3]:
             name = items[3]
-            count = tas.get(name, 0)
-            tas[name] = count + 1
-        else:
-            continue
+            tas[name] = tas.get(name, {"count": 0, "section": "C"})
+            tas[name]["count"] += 1
+    
+    # Process online submissions
+    with open(onlineFile) as infile:
+        soup = BeautifulSoup(infile, "html.parser")
+
+    rows = [row for row in soup.find_all("tr")]
+    for row in rows:
+        items = [item.text for item in row.find_all("td")]
+        if len(items) > 3 and items[3]:
+            name = items[3]
+            tas[name] = tas.get(name, {"count": 0, "section": "O"})
+            tas[name]["count"] += 1
     return tas
 
 def calculateStats(submissionDict, efficiencyDict):
+     # tas = {name: {percent: 0, section: "O"}}
     tas = {}
     totalList = []
     for name in submissionDict:
-        numGraded = submissionDict[name]
-        numEfficient = efficiencyDict.get(name, 0)
+        numGraded = submissionDict[name]["count"]
+        if name in efficiencyDict.keys():
+            numEfficient = efficiencyDict[name]["count"]
+        else:
+            numEfficient = 0
         percent = round(numEfficient / numGraded, 4)
-        tas[name] = percent
+        section = submissionDict[name]["section"]
+        tas[name] = {"percent": percent, "section": section}
         totalList.append(percent)
     
     arr = np.array(totalList)
@@ -49,15 +82,42 @@ def calculateStats(submissionDict, efficiencyDict):
     stdev = round(np.std(totalList), 4)
     return tas, mean, stdev
 
-def writeToExcel(tas, mean, stdev):
+def writeToCSV(tas, mean, stdev, newFile):
+    campusList = []
+    onlineList = []
 
+    for name in tas:
+        formattedName = formatName(name)
+        percent = tas[name]["percent"]
+        if tas[name]["section"] == "C":
+            campusList.append([formattedName, percent])
+        else:
+            onlineList.append([formattedName, percent])
+    campusList.sort()
+
+    with open(newFile, "w") as fout:
+        writer = csv.writer(fout)
+        writer.writerows(campusList)
+        writer.writerow([])
+        writer.writerows(onlineList)
+
+def formatName(name):
+    if "," in name:
+        parts = name.split(",")
+        name = f"{parts[1]} {parts[0]}".strip()
+    return name
+
+def main(semester, homework):
+    submissionDict = countSubmissions(os.path.join(semester, homework, "campusSubmissions.html"),
+                                      os.path.join(semester, homework, "onlineSubmissions.html"))
+    efficiencyDict = countEfficiencies(os.path.join(semester, homework, "campusEfficiency.html"),
+                                      os.path.join(semester, homework, "onlineEfficiency.html"))
+    tas, mean, stDev = calculateStats(submissionDict, efficiencyDict)
+    writeToCSV(tas, mean, stDev, "output.csv")
 
     
 if __name__ == "__main__":
-    submissionDict = countSubmissions("submissions.html")
-    efficiencyDict = countEfficiencies("efficiency.html")
-    tas, mean, stdev = calculateStats(submissionDict, efficiencyDict)
-    writeToExcel(tas, mean, stdev)
+    main("spring2024", "hw5")
     
     
    
